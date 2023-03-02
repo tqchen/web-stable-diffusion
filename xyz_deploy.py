64e8696d9eecc7829632c9386cb8e85a2b1bfeec
tqchen <tqchenml@gmail.com>
2023-03-01 21:19:47 -0500

    OK

--- xyz_deploy.py
+++ xyz_deploy.py
@@ -71,15 +71,15 @@ def deploy(lib_path, cache_path, mode):
         vm = relax.VirtualMachine(ex, dev)
         return  MetalWrapper(vm, param_dict, dev)
 
-    # wgpu_wrapper = WebGPUWrapper("build/vae.wasm")
+    wgpu_wrapper = WebGPUWrapper("build/vae.wasm")
     metal_wrapper = get_metel_wrapper(tvm.metal())
 
-    wrapper = metal_wrapper
+    wrapper = wgpu_wrapper
     # wrapper.clip = metal_wrapper.clip
     # wrapper.unet = metal_wrapper.unet
-    wrapper.scheduler = metal_wrapper.scheduler
+    # wrapper.scheduler = metal_wrapper.scheduler
 
-    # wrapper.vae = metal_wrapper.vae
+    #wrapper.vae = metal_wrapper.vae
 
     pipe = TVMSDPipeline(
         wrapper,
