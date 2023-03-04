072369ef0c20c9715fa7b66690179f24a529679d
tqchen <tqchenml@gmail.com>
2023-03-04 09:03:46 -0500

    OK

--- xyz_build.py
+++ xyz_build.py
@@ -4,6 +4,7 @@ import argparse
 import numpy as np
 
 import tvm
+import tvmjs
 import tvm.testing
 from tvm import meta_schedule as ms, relax
 import tvm.tir.tensor_intrin
@@ -28,10 +29,22 @@ def build(
     ex = relax.build(relax_mod, target)
     ex.mod.export_library(output)
 
+def build_webgpu():
+    wasm_path = "build/stable-diffusion.wasm"
+    import webgpu_module
+    relax_mod = webgpu_module.Module
+    target = tvm.target.Target("webgpu", host="llvm -mtriple=wasm32-unknown-unknown-wasm")
+    ex = relax.build(relax_mod, target)
+    ex.export_library(wasm_path, tvmjs.create_tvmjs_wasm)
+    print("finish export")
+
 
 if __name__ == "__main__":
     ARGS = _parse_args()
-    build(
-        ARGS.target,
-        ARGS.output,
-    )
+    if ARGS.target == "webgpu":
+        build_webgpu()
+    else:
+        build(
+            ARGS.target,
+            ARGS.output,
+        )
